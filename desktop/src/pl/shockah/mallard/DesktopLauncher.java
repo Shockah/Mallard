c41164e3d09ac3459edac6e1ae05c136d5ea405d
Michał Dolaś <me@shockah.pl>
2018-06-13 13:24:29 +0200

    Switch to LWJGL2 in preparation for file chooser usage

--- desktop/src/pl/shockah/mallard/DesktopLauncher.java
+++ desktop/src/pl/shockah/mallard/DesktopLauncher.java
@@ -1,18 +1,20 @@
 package pl.shockah.mallard;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import pl.shockah.godwit.PlatformGodwitAdapter;
 
 public class DesktopLauncher {
 	public static void main(String[] args) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Mallard");
-		config.setWindowedMode(1334, 750);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Mallard";
+		config.fullscreen = false;
+		config.width = 1334;
+		config.height = 750;
 		//config.useVsync(false);
 		//config.hdpiMode = Lwjgl3ApplicationConfiguration.HdpiMode.Pixels
-		new Lwjgl3Application(new PlatformGodwitAdapter(EditorState::new) {
+		new LwjglApplication(new PlatformGodwitAdapter(EditorState::new) {
 			@Override
 			public void create() {
 				super.create();

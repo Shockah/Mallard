073c3411b5bbd5f41a21011dd19ff69592a01454
Michał Dolaś <me@shockah.pl>
2018-07-04 11:49:24 +0200

    Refactor

--- desktop/src/pl/shockah/mallard/ui/controller/MenuBarController.java
+++ desktop/src/pl/shockah/mallard/ui/controller/MenuBarController.java
@@ -8,28 +8,26 @@ import javafx.scene.control.MenuItem;
 
 public class MenuBarController extends Controller {
 	@Nonnull
-	public final MenuBar menuBar;
-
-	@Nonnull
 	public final AppController appController;
 
 	public MenuBarController(@Nonnull AppController appController) {
 		this.appController = appController;
-		setView(menuBar = new MenuBar());
-		menuBar.setUseSystemMenuBar(true);
+		setView(new MenuBar() {{
+			setUseSystemMenuBar(true);
 
-		menuBar.getMenus().add(new Menu("File") {{
-			getItems().add(new MenuItem("New Project") {{
-				setOnAction(event -> appController.newAction());
-			}});
-			getItems().add(new MenuItem("Open") {{
-				setOnAction(event -> appController.openAction());
-			}});
-			getItems().add(new MenuItem("Save") {{
-				setOnAction(event -> appController.saveAction());
-			}});
-			getItems().add(new MenuItem("Save As...") {{
-				setOnAction(event -> appController.saveAsAction());
+			getMenus().add(new Menu("File") {{
+				getItems().add(new MenuItem("New Project") {{
+					setOnAction(event -> appController.newAction());
+				}});
+				getItems().add(new MenuItem("Open") {{
+					setOnAction(event -> appController.openAction());
+				}});
+				getItems().add(new MenuItem("Save") {{
+					setOnAction(event -> appController.saveAction());
+				}});
+				getItems().add(new MenuItem("Save As...") {{
+					setOnAction(event -> appController.saveAsAction());
+				}});
 			}});
 		}});
 	}

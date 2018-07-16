504cc352971d3fbb71b85695d6715f83b4cd02ca
Michał Dolaś <me@shockah.pl>
2018-07-16 10:48:03 +0200

    MallardSprite.[Subsprite/Animation.Frame].getGfxSprite(); MallardSprite.Animation.getFrame(); MallardSprite.Animation.asFx()

--- core/src/pl/shockah/mallard/MallardSprite.java
+++ core/src/pl/shockah/mallard/MallardSprite.java
@@ -1,17 +1,29 @@
 package pl.shockah.mallard;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nonnull;
 
+import java8.util.Spliterators;
+import java8.util.stream.RefStreams;
+import java8.util.stream.StreamSupport;
+import pl.shockah.godwit.fx.Fx;
+import pl.shockah.godwit.fx.RunnableFx;
+import pl.shockah.godwit.fx.SequenceFx;
+import pl.shockah.godwit.fx.WaitFx;
 import pl.shockah.godwit.geom.Shape;
 import pl.shockah.godwit.geom.Vec2;
+import pl.shockah.godwit.gl.GfxSprite;
 import pl.shockah.unicorn.collection.Array1D;
+import pl.shockah.unicorn.func.Action1;
 
 public class MallardSprite {
 	public static final int VERSION = 1;
@@ -51,6 +63,14 @@ public class MallardSprite {
 			this.origin = origin;
 			this.shapes = Collections.unmodifiableMap(new LinkedHashMap<>(shapes));
 		}
+
+		@Nonnull
+		public GfxSprite getGfxSprite() {
+			GfxSprite sprite = new GfxSprite(new Sprite(region));
+			sprite.setOrigin(origin);
+			sprite.offset.set(origin);
+			return sprite;
+		}
 	}
 
 	public static class Animation {
@@ -69,6 +89,32 @@ public class MallardSprite {
 			this.duration = duration;
 		}
 
+		@Nonnull
+		public Frame getFrame(float f) {
+			float sum = (float)StreamSupport.stream(Spliterators.spliteratorUnknownSize(frames.iterator(), 0), false)
+					.mapToDouble(frame -> frame.relativeDuration)
+					.sum();
+			float find = f * sum;
+
+			for (Frame frame : frames) {
+				if (find < frame.relativeDuration)
+					return frame;
+				find -= frame.relativeDuration;
+			}
+			return frames.get(frames.length - 1);
+		}
+
+		@Nonnull
+		public Fx asFx(@Nonnull Action1<Frame> func) {
+			List<Fx> list = new ArrayList<>();
+			float sum = (float)RefStreams.of(frames).mapToDouble(frame -> frame.length).sum();
+			for (Frame frame : frames) {
+				list.add(new RunnableFx(() -> func.call(frame)));
+				list.add(new WaitFx(duration * frame.relativeDuration / sum));
+			}
+			return new SequenceFx(list);
+		}
+
 		public static class Frame {
 			@Nonnull
 			public final Subsprite subsprite;
@@ -82,6 +128,11 @@ public class MallardSprite {
 				this.subsprite = subsprite;
 				this.relativeDuration = relativeDuration;
 				this.offset = offset;
+			}
+
+			@Nonnull
+			public GfxSprite getGfxSprite() {
+				return subsprite.getGfxSprite();
 			}
 		}
 	}
